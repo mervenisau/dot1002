f2ff154d8e65b6626f4b80efd0bb4dba149102af
mervenisau <mervenisau@gmail.com>
2026-04-27 11:22:25 +0300

    Add ISP exercise solution and UML

---
+++ Week10-ISP-Exercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public interface IAttackable
+{
+    void Attack();
+}
+
+public interface IReloadable
+{
+    void Reload();
+}
+
+public interface IAimable
+{
+    void AimDownSights();
+}
+
+public class SniperRifle : IAttackable, IReloadable, IAimable
+{
+    public void Attack()
+    {
+        Console.WriteLine("Firing a high-caliber round!");
+    }
+
+    public void Reload()
+    {
+        Console.WriteLine("Loading a new magazine.");
+    }
+
+    public void AimDownSights()
+    {
+        Console.WriteLine("Looking through the 8x scope.");
+    }
+}
+
+public class Sword : IAttackable
+{
+    public void Attack()
+    {
+        Console.WriteLine("Swinging the sword!");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        SniperRifle sniperRifle = new SniperRifle();
+        Sword sword = new Sword();
+
+        sniperRifle.Attack();
+        sniperRifle.Reload();
+        sniperRifle.AimDownSights();
+
+        sword.Attack();
+    }
+}
